18658c9c73127728ec33b2585e8475f4abcddb28
Cole Koryto <ckoryto@alarm.com>
2024-07-02 16:22:06 -0400

    Setup of frontend

---
+++ src/components/AddableInputBox.tsx
@@ -0,0 +1,76 @@
+import React, { useState, useRef } from 'react'
+import ReactDOM from "react-dom";
+//https://codesandbox.io/s/react-dynamic-form-xez6d?file=/src/App.js:230-299
+
+//@ts-ignore
+const AddableInputBox = ({fields, setFields}) => {
+    
+
+    const fieldRef = useRef();
+
+    const fieldsIsValid = fields.length >= 1 && fields.every((field: any) => field.trim() !== "");
+
+    function handleChange(i: number, event: any) {
+        const values = [...fields];
+        values[i] = event.target.value;
+        setFields(values);
+    }
+
+    function handleAdd() {
+        const values = [...fields];
+        values.push("");
+        setFields(values);
+    }
+
+    function handleRemove(i: number) {
+        const values = [...fields];
+        values.splice(i, 1);
+        setFields(values);
+    }
+
+
+    function submitHandler(event: any) {
+        event.preventDefault();
+    
+        if (!fieldsIsValid) {
+            if (fields.length >= 1) 
+            {
+                //@ts-ignore
+                fieldRef.current.focus();
+                return;
+            }
+          return;
+        }
+        console.log(fields);
+      }
+
+    return (
+        <div className="App">
+
+            <button type="button" onClick={() => handleAdd()}>
+              Add Input
+            </button>
+            {!fieldsIsValid && <p className="error">Input is required</p>}
+            {fields.map((field: any, idx: any) => {
+              return (
+                <div key={`${"asdf"}-${idx}`}>
+                    <span className="flex">
+                        <input
+                            type="text"
+                            placeholder="Enter percentile"
+                            value={field || ""}
+                            className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            onChange={(e) => handleChange(idx, e)}
+                        />
+                        <button type="button" onClick={() => handleRemove(idx)}>
+                            X
+                        </button>
+                    </span>
+                </div>
+              );
+            })}
+        </div>
+    )
+}
+
+export default AddableInputBox;
---
+++ src/components/InputBox.tsx
@@ -0,0 +1,94 @@
+import React, { useState, FC } from 'react'
+import AddableInputBox from './AddableInputBox'
+
+
+// interface MyInputProps 
+// {
+//     inputNameArray: Array<string>;
+// }
+
+
+// const InputBox = ({inputNameArray}: MyInputProps) => {
+//   return (
+//     <div>
+//         <form className="max-w-sm mx-auto">
+//             {inputNameArray.map((item, ) => (
+//                 <div className="mb-5">
+//                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{item}</label>
+//                     <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+//                 </div> 
+//             ))}
+//         </form>
+//     </div>
+//   )
+// }
+
+
+const handleSubmit = (event: any) => {
+    event.preventDefault()
+    console.log("current_balance=" + event.target.current_balance.value)
+    console.log("annual_return=" + event.target.annual_return.value)
+    console.log("return_std=" + event.target.return_std.value)
+    console.log("current_age=" + event.target.current_age.value)
+    console.log("life_expectancy=" + event.target.life_expectancy.value)
+    console.log("inflation=" + event.target.inflation.value)
+    //console.log(fields)
+    console.log("distribution_type=" + event.target.distribution_type.value)
+    console.log("random_state=" + event.target.random_state.value)
+}
+
+
+const InputBox = () => {
+    const [fields, setFields] = useState([""]);
+
+    return (
+        <div>
+            <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+                <div className="mb-5">
+                    <label htmlFor="current_balance" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current Balance</label>
+                    <input name="current_balance" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                </div>
+                <div className="mb-5">
+                    <label htmlFor="annual_return" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Annual Return</label>
+                    <input name="annual_return" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                </div>
+                <div className="mb-5">
+                    <label htmlFor="return_std" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Return Standard Deviation</label>
+                    <input name="return_std" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                </div>
+                <div className="mb-5">
+                    <label htmlFor="current_age" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current Age</label>
+                    <input name="current_age" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                </div>
+                <div className="mb-5">
+                    <label htmlFor="life_expectancy" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Life Expectancy</label>
+                    <input name="life_expectancy" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                </div>
+                <div className="mb-5">
+                    <label htmlFor="inflation" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Inflation</label>
+                    <input name="inflation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                </div>
+                <div className="mb-5">
+                    <label htmlFor="num_simulations" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number of Simulations</label>
+                    <input name="num_simulations" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                </div>
+                <div className="mb-5">
+                    <AddableInputBox fields={fields} setFields={setFields}/>
+                </div>
+                <label htmlFor="distribution_type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Distribution Type</label>
+                <select name="distribution_type" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option value="normal">Normal</option>
+                    <option value="laplace">Laplace</option>
+                </select>
+                <div className="mb-5">
+                    <label htmlFor="random_state" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Random State</label>
+                    <input name="random_state" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                </div>
+
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default InputBox
