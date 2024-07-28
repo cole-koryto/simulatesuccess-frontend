7727a8fd428eebf96e7aa07cb2973f4809ba9b42
colek <colekoryto17@gmail.com>
2024-07-28 01:23:50 -0400

    Add title

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,22 @@ function App() {
   const [simulationInputs, setSimulationInputs] = useState()
 
   return (
-    <div className="flex flex-col md:flex-row m-0 p-6 text-center">
-      <div className="w-full md:w-1/2 m-4">
-        <InputBox setSimulationInputs={setSimulationInputs} setSimulationData={setSimulationData} />
-        {simulationData && <ExportData data={simulationInputs} displayText="Export Simulation Inputs to JSON" fileName="simulation_inputs.json" />}
-        {simulationData && <ExportData data={simulationData} displayText="Export Results to JSON" fileName="simulation_results.json" />}
-        {simulationData && <SimulationSummary simulationSummary={simulationData["simulation_summary"]} />}
+    <div>
+      <div className="bg-gray-700 p-4 m-4 rounded">
+          <h1 className="text-3xl font-bold text-white text-center">Simulate Success - Monte Carlo Retirement Calculator</h1>
       </div>
-      <div className="w-full md:w-1/2 m-4">
-        {simulationData && <PercentileGraph percentileHistory={simulationData["percentile_balance_history"]} />}
-        {simulationData && <CashFLowsTable TotalIncomeData={simulationData["income_by_year"]} TotalSpendingData={simulationData["spending_by_year"]} NetIncomeData={simulationData["net_income_by_year"]} />}
+      <div className="flex flex-col md:flex-row p-2 text-center">
+        
+        <div className="w-full md:w-1/2 m-2">
+          <InputBox setSimulationInputs={setSimulationInputs} setSimulationData={setSimulationData} />
+          {simulationData && <ExportData data={simulationInputs} displayText="Export Simulation Inputs to JSON" fileName="simulation_inputs.json" />}
+          {simulationData && <ExportData data={simulationData} displayText="Export Results to JSON" fileName="simulation_results.json" />}
+          {simulationData && <SimulationSummary simulationSummary={simulationData["simulation_summary"]} />}
+        </div>
+        <div className="w-full md:w-1/2 m-2">
+          {simulationData && <PercentileGraph percentileHistory={simulationData["percentile_balance_history"]} />}
+          {simulationData && <CashFLowsTable TotalIncomeData={simulationData["income_by_year"]} TotalSpendingData={simulationData["spending_by_year"]} NetIncomeData={simulationData["net_income_by_year"]} />}
+        </div>
       </div>
     </div>
   )
