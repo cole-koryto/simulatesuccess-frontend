f646b61f729494a8587fd8b57a9b5ceb1b5e272d
colek <colekoryto17@gmail.com>
2024-07-28 01:32:54 -0400

    Fix padding on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,15 @@ function App() {
       <div className="bg-gray-700 p-4 m-4 rounded">
           <h1 className="text-3xl font-bold text-white text-center">Simulate Success - Monte Carlo Retirement Calculator</h1>
       </div>
-      <div className="flex flex-col md:flex-row p-2 text-center">
+      <div className="flex flex-col md:flex-row text-center">
         
-        <div className="w-full md:w-1/2 m-2">
+        <div className="w-full md:w-1/2 p-4">
           <InputBox setSimulationInputs={setSimulationInputs} setSimulationData={setSimulationData} />
           {simulationData && <ExportData data={simulationInputs} displayText="Export Simulation Inputs to JSON" fileName="simulation_inputs.json" />}
           {simulationData && <ExportData data={simulationData} displayText="Export Results to JSON" fileName="simulation_results.json" />}
           {simulationData && <SimulationSummary simulationSummary={simulationData["simulation_summary"]} />}
         </div>
-        <div className="w-full md:w-1/2 m-2">
+        <div className="w-full md:w-1/2 p-4">
           {simulationData && <PercentileGraph percentileHistory={simulationData["percentile_balance_history"]} />}
           {simulationData && <CashFLowsTable TotalIncomeData={simulationData["income_by_year"]} TotalSpendingData={simulationData["spending_by_year"]} NetIncomeData={simulationData["net_income_by_year"]} />}
         </div>
